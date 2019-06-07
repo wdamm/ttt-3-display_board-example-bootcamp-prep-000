bd7eee7b2794ac5d0532241356e0111392c3cf1f
William Damm <wdamm@powerhrg.com>
2019-06-07 20:47:58 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,6 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
   puts "   |   |   "
-  puts "___________"
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
